841c0c6edd4bdba89230e8f6fbb73927008b7f9e
Stepan_Makarenko <makarenko.sd@phystech.edu>
2023-12-07 22:05:13 +0100

    Add gaussian environment Add UCB1 agent

--- multi_arm.py
+++ multi_arm.py
@@ -46,3 +46,45 @@ class MultiArmedBanditEnv(Env):
 
     def close(self) -> None:
         pass
+
+
+class MultiArmedGausianBanditEnv(Env):
+    def __init__(self, mus: List[float], sigmas: List[float], max_steps: int = 10000, seed: Optional[int] = None) -> None:
+        assert len(mus) == len(sigmas), f'mus and sigmas have different sizes - {len(mus)} and {len(sigmas)}'
+        super().__init__()
+        self.n_arms = len(mus)
+        self.max_steps = max_steps
+        self.curr_step = 0
+        self.action_space = spaces.Discrete(self.n_arms)
+        self.observation_space = spaces.Discrete(1)  # No real observations, just a dummy space
+
+        self.seed(seed)
+        self.mus = mus
+        self.sigmas = sigmas
+        self.opt_action = np.argmax(self.mus)
+
+    def seed(self, seed: Optional[int] = None) -> List[int]:
+        self.np_random, seed = utils.seeding.np_random(seed)
+        return [seed]
+
+    def step(self, action: int) -> Tuple[int, int, bool, dict]:
+        assert self.action_space.contains(action), f"{action} ({type(action)}) invalid"
+        self.curr_step += 1
+        
+        # Reward is 1 with probability of the chosen arm, otherwise 0
+        reward =  self.np_random.normal(self.mus[action], self.sigmas[action])
+        done = self.max_steps < self.curr_step
+        return 0, reward, done, {}
+
+    def get_optimal_action(self):
+        return self.opt_action
+
+    def reset(self) -> int:
+        self.curr_step = 0
+        return 0  # Resetting a bandit environment does not change its state
+
+    def render(self, mode: str = 'human', close: bool = False) -> None:
+        pass  # Rendering is not needed for this simple environment
+
+    def close(self) -> None:
+        pass
